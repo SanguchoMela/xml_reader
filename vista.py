2176bacc7840872d87e365f6f7f3f73b84b7c665
SanguchoMela <melany01-03@hotmail.com>
2025-01-03 19:03:27 -0500

    feat: procesar retenciones

--- vista.py
+++ vista.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from tkinter import filedialog
 
-import lectorfact
+# import lectorfact
 from lectorfact import procesar_carpeta_facturas, exportar_facturas_excel, data_facturas
+
+from lectorRet import procesar_carpeta_retenciones, exportar_retenciones_excel, data_retenciones
 
 # Definicion de ventana principal
 window = tk.Tk()
@@ -13,18 +15,19 @@ frame_superior = tk.Frame(window)
 frame_superior.pack(fill=tk.BOTH, expand=True)
 # frame_superior.pack(side=tk.TOP, fill=tk.X)
 
-frame_facturas = tk.Frame(window)
-frame_facturas.pack(fill=tk.BOTH, expand=True)
-# frame_facturas.pack(side=tk.BOTTOM, fill=tk.X)
+frame_proceso = tk.Frame(window)
+frame_proceso.pack(fill=tk.BOTH, expand=True)
+# frame_proceso.pack(side=tk.BOTTOM, fill=tk.X)
 
 def limpiarFrame(frame):
     # Reiniciar variables
-    global rutaCarpeta, rutaExcel, data_facturas
+    global rutaCarpeta, rutaExcel, data_facturas, data_retenciones
     rutaCarpeta = ""
     rutaExcel = ""
     data_facturas.clear()
+    data_retenciones.clear()
     # Eliminar widgets
-    for widget in frame_facturas.winfo_children():
+    for widget in frame_proceso.winfo_children():
         widget.destroy()
 
 # Funcion para abrir el explorador de archivos
@@ -32,18 +35,26 @@ def abrirCarpeta():
     global rutaCarpeta
     rutaCarpeta = filedialog.askdirectory()
     if rutaCarpeta:
-        labelCarpeta = tk.Label(frame_facturas, text="La carpeta que seleccionaste es: "+rutaCarpeta)
+        labelCarpeta = tk.Label(frame_proceso, text="La carpeta que seleccionaste es: "+rutaCarpeta)
         labelCarpeta.pack()
         # print("Espera mientras se procesa la carpeta")
-        procesar_carpeta_facturas(rutaCarpeta)
-        labelGuardar = tk.Label(frame_facturas, text="Proceso completo, elige el lugar para guardar tu resumen")
+        print(f"{isFactura}")
+        if isFactura: 
+            procesar_carpeta_facturas(rutaCarpeta)
+        else:
+            procesar_carpeta_retenciones(rutaCarpeta)
+        
+        labelGuardar = tk.Label(frame_proceso, text="Proceso completo, elige el lugar para guardar tu resumen")
         labelGuardar.pack()
         
-        buttonSave = tk.Button(frame_facturas,text="Guardar como", width=20, command=guardarExcelResumen)
+        buttonSave = tk.Button(frame_proceso,text="Guardar como", width=20, command=guardarExcelResumen)
         buttonSave.pack()
     else:
-        labelCarpeta = tk.Label(frame_facturas, text="No seleccionaste ninguna carpeta")
+        labelCarpeta = tk.Label(frame_proceso, text="No seleccionaste ninguna carpeta")
         labelCarpeta.pack()
+        
+        limpiarButton = tk.Button(frame_proceso, text="Limpiar", command=lambda:limpiarFrame(frame_proceso))
+        limpiarButton.pack()
         # print("No seleccionaste ninguna carpeta")
 
 # Funcion para guardar los archivos xml ya procesados
@@ -52,31 +63,44 @@ def guardarExcelResumen():
     rutaExcel = filedialog.asksaveasfilename()
     # print("Ruta excel: "+rutaExcel)
     if rutaExcel:
-        exportar_facturas_excel(data_facturas, rutaExcel)
-        labelGuardarExcel = tk.Label(frame_facturas, text="Tu resumen se guardará en: "+rutaExcel)
+        if isFactura:
+            exportar_facturas_excel(data_facturas, rutaExcel)
+        else:
+            exportar_retenciones_excel(data_retenciones, rutaExcel)
+            
+        labelGuardarExcel = tk.Label(frame_proceso, text="Tu resumen se guardará en: "+rutaExcel)
         labelGuardarExcel.pack()
         # print("Tu resumen se guardará en: "+rutaExcel)
         
         # Borrar el frame de facturas
-        limpiarButton = tk.Button(frame_facturas, text="Limpiar", command=lambda:limpiarFrame(frame_facturas))
+        limpiarButton = tk.Button(frame_proceso, text="Limpiar", command=lambda:limpiarFrame(frame_proceso))
         limpiarButton.pack()
     else:
-        labelGuardarExcel = tk.Label(frame_facturas, text="No seleccionaste ninguna lugar")
+        labelGuardarExcel = tk.Label(frame_proceso, text="No seleccionaste ningun lugar")
         labelGuardarExcel.pack()
+        
+        limpiarButton = tk.Button(frame_proceso, text="Limpiar", command=lambda:limpiarFrame(frame_proceso))
+        limpiarButton.pack()
         # print("No seleccionaste ninguna carpeta")
 
 # Funcion para abrir un Toplevel al presionar un boton
 def seccionFacturas():
-    # print("Ventana facturas")
-    # ventanaFact = Toplevel()
-    # ventanaFact.title("Lector de facturas")
-    # ventanaFact.geometry("400x250")
-    
-    labelSelect = tk.Label(frame_facturas,text="Selecciona la carpeta con facturas en xml")
+    global isFactura
+    labelSelect = tk.Label(frame_proceso,text="Selecciona la carpeta con facturas en xml")
     labelSelect.pack()
     
-    buttonSelect = tk.Button(frame_facturas,text="Seleccionar", width=20, command=abrirCarpeta)
+    buttonSelect = tk.Button(frame_proceso,text="Seleccionar", width=20, command=abrirCarpeta)
     buttonSelect.pack()
+    isFactura = True
+
+def seccionRetenciones():
+    global isFactura
+    labelSelect = tk.Label(frame_proceso,text="Selecciona la carpeta con retenciones en xml")
+    labelSelect.pack()
+    
+    buttonSelect = tk.Button(frame_proceso,text="Seleccionar", width=20, command=abrirCarpeta)
+    buttonSelect.pack()
+    isFactura = False
 
 
 # Estilizado de la ventana principal
@@ -86,8 +110,8 @@ labelProgram.pack()
 botonFact = tk.Button(frame_superior, text="Facturas", width=25, command=seccionFacturas)
 botonFact.pack()
 
-# botonRet = tk.Button(window, text="Retenciones", width=25)
-# botonRet.pack()
+botonFact = tk.Button(frame_superior, text="Retenciones", width=25, command=seccionRetenciones)
+botonFact.pack()
 
 # Para mostrar la ventana principal
 window.mainloop()
